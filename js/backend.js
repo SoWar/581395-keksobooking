e53bcff0c37d4e64392215ed8b19409e465d8dba
SoWar <junky.man@yandex.ru>
2017-12-21 16:33:52 +0300

    загрузка данных с сервера и прототип модального окна без событий

---
+++ js/backend.js
@@ -0,0 +1,79 @@
+'use strict';
+
+(function () {
+
+  var LOAD_URL = 'https://js.dump.academy/keksobooking/data';
+  var SEND_URL = 'https://js.dump.academy/keksobooking';
+
+
+  // var onResponse = function (xhr) {
+  //   var error;
+  //   switch (xhr.status) {
+  //     case 200:
+  //       onLoad(xhr.response);
+  //       break;
+  //     case 400:
+  //       error = 'Неверный запрос';
+  //       break;
+  //     case 401:
+  //       error = 'Пользователь не авторизован';
+  //       break;
+  //     case 404:
+  //       error = 'Ничего не найдено';
+  //       break;
+  //     default:
+  //       error = 'Неизвестный статус: ' + xhr.status + ' ' + xhr.statusText;
+  //   }
+  //
+  //   // if (error) {
+  //   //   onError(error);
+  //   // }
+  // };
+  var addRequestListners = function (xhr, onLoad, onError) {
+    xhr.addEventListener('load', function () {
+      var error;
+      switch (xhr.status) {
+        case 200:
+          onLoad(xhr.response);
+          break;
+        case 400:
+          error = 'Неверный запрос';
+          break;
+        case 401:
+          error = 'Пользователь не авторизован';
+          break;
+        case 404:
+          error = 'Ничего не найдено';
+          break;
+        default:
+          error = 'Неизвестный статус: ' + xhr.status + ' ' + xhr.statusText;
+      }
+
+      if (error) {
+        onError(error);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+  };
+
+  window.backend = {
+    load: function (onLoad, onError) {
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+
+      addRequestListners(xhr, onLoad, onError);
+
+      xhr.timeout = 10000;
+
+      xhr.open('GET', LOAD_URL);
+      xhr.send();
+    }
+  };
+})();
